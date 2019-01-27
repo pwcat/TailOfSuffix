3a245fae6d564445be9ed7289aded88ce96b53bb
pwcat <pwcat@users.noreply.github.com>
2019-01-27 22:46:49 +0800

    initial

--- app/src/main/java/com/pwcat/root/tailofsuffix/MainActivity.java
+++ app/src/main/java/com/pwcat/root/tailofsuffix/MainActivity.java
@@ -2,12 +2,26 @@ package com.pwcat.root.tailofsuffix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText tail_et;
+    Button pickPath_btn, commit_btn;
+    String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+        tail_et = findViewById(R.id.tail_et);
+        pickPath_btn = findViewById(R.id.pickpath_btn);
+        commit_btn = findViewById(R.id.commit_btn);
     }
 }
